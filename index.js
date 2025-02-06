8a52a99a4e301049974d26f7b0fd015170f1a2a7
Riya Saxena <121282155+riyagh@users.noreply.github.com>
2025-02-07 00:58:27 +0530

    Complete dice game

---
+++ index.js
@@ -0,0 +1,19 @@
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomImageSource1 = "images/dice" + randomNumber1 + ".png";
+
+let image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource1);
+
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+let image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomImageSource2);
+
+if (randomImageSource1 > randomImageSource2) {
+  document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+} else if (randomImageSource1 < randomImageSource2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
